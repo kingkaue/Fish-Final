3e8925c8f2a7951460ba625430ae46550c57e6fe
kingkaue <kaueking@gmail.com>
2025-03-08 01:25:28 -0500

    Player movement and camera
    Implemented player movement controls for K + M and controller

---
+++ Assets/Scripts/CameraBehavior.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class CameraBehavior : MonoBehaviour
+{
+    public Vector3 CamOffset;
+    public float smoothTime = 0.3f;
+    private Transform player;
+    private Vector3 velocity = Vector3.zero;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (player != null)
+        {
+            Vector3 targetPosition = player.position + CamOffset;
+
+            transform.position = Vector3.SmoothDamp(transform.position, targetPosition, ref velocity, smoothTime);
+
+            transform.LookAt(player);
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,107 @@
+using Unity.Mathematics;
+using UnityEngine;
+using UnityEngine.AI;
+using UnityEngine.InputSystem;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float speed;
+    private Vector2 move;
+    private Vector2 mouseLook;
+    private Vector2 joystickLook;
+    private Vector3 rotationTarget;
+    public bool isPC;
+
+    public void OnMove(InputAction.CallbackContext context)
+    {
+        move = context.ReadValue<Vector2>();
+    }
+
+    public void OnMouseLook(InputAction.CallbackContext context)
+    {
+        mouseLook = context.ReadValue<Vector2>();
+    }
+
+    public void OnJoystickLook(InputAction.CallbackContext context)
+    {
+        joystickLook = context.ReadValue<Vector2>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // If using mouse and keyboard, sets rotation target to mouse
+        if (isPC)
+        {
+            RaycastHit hit;
+            
+            // Creates ray pointing to mouse locatiin
+            Ray ray = Camera.main.ScreenPointToRay(mouseLook);
+
+            // Rotates towards mouse pointer
+            if (Physics.Raycast(ray, out hit))
+            {
+                rotationTarget = hit.point;
+            }
+
+            MovePlayerWithAim();
+        }
+        // If using controller
+        else
+        {
+            // If no input on right joystick, rotate with left stick
+            if (joystickLook.x == 0 && joystickLook.y == 0)
+            {
+                MovePlayer();
+            }
+            else
+            {
+                MovePlayerWithAim();
+            }
+        }
+    }
+
+    // Movement and rotation handled by left joystick
+    public void MovePlayer()
+    {
+        Vector3 movement = new Vector3(move.x, 0f, move.y);
+
+        if (movement != Vector3.zero)
+        {
+            transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(movement), 0.15f);
+        }
+
+        transform.Translate(movement * speed * Time.deltaTime, Space.World);
+    }
+
+    // Movement and rotation handled separately
+    public void MovePlayerWithAim()
+    {
+        if (isPC)
+        {
+            var lookPos = rotationTarget - transform.position;
+            lookPos.y = 0;
+            var rotation = Quaternion.LookRotation(lookPos);
+
+            Vector3 aimDirection = new Vector3(rotationTarget.x, 0f, rotationTarget.z);
+
+            if (aimDirection != Vector3.zero)
+            {
+                transform.rotation = Quaternion.Slerp(transform.rotation, rotation, 0.15f);
+            }
+        }
+        else
+        {
+            Vector3 aimDirection = new Vector3(joystickLook.x, 0f, joystickLook.y);
+
+            if (aimDirection != Vector3.zero)
+            {
+                transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(aimDirection), 0.15f);
+            }
+        }
+
+        Vector3 movement = new Vector3(move.x, 0f, move.y);
+
+        transform.Translate(movement * speed * Time.deltaTime, Space.World);
+    }
+}
