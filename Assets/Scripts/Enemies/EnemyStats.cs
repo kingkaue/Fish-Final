d00a049fc031fa109784f735e1d65c5236ca8775
mounirziad <mo697861@ucf.edu>
2025-03-11 11:45:16 -0400

    FixedConsumable

--- Assets/Scripts/Enemies/EnemyStats.cs
+++ Assets/Scripts/Enemies/EnemyStats.cs
@@ -6,6 +6,7 @@ public class EnemyStats : MonoBehaviour
     [SerializeField] private float _maxHealth = 3;
     [SerializeField] private float _currentHealth;
     [SerializeField] private HealthBarScript _healthbar;
+    public GameObject collectable;
 
     void Start()
     {
@@ -20,6 +21,19 @@ public class EnemyStats : MonoBehaviour
             _currentHealth = Mathf.Max(_currentHealth - 1, 0); // Ensure health doesn't go below 0
             _healthbar.UpdateHealthBar(_maxHealth, _currentHealth); // Update health bar
             Debug.Log("Projectile Collision With Enemy");
+        }
+    }
+
+    private void LateUpdate()
+    {
+        if(_currentHealth == 0)
+        {
+            GameObject pickup = Instantiate(collectable, transform.position, Quaternion.identity);
+            Vector3 collectablePosition = transform.position;
+            collectablePosition.x = transform.position.x;
+            collectablePosition.z = transform.position.z;
+            pickup.transform.position = collectablePosition;
+            Destroy(gameObject);
         }
     }
 }
