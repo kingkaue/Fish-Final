0d7926482ccbf1b8610e000c84fad88d296a8b74
mounirziad <mo697861@ucf.edu>
2025-03-15 15:11:04 -0400

    DeathAnimationsNewEnemy

--- Assets/Scripts/Enemies/EnemyStats.cs
+++ Assets/Scripts/Enemies/EnemyStats.cs
@@ -1,29 +1,36 @@
+using System.Collections;
 using UnityEngine;
-using UnityEngine.UI;
-using static UnityEngine.GraphicsBuffer;
+
 public class EnemyStats : MonoBehaviour
 {
     public float _maxHealth = 10;
     public float _currentHealth;
     public HealthBarScript _healthbar;
     public GameObject collectable;
+    private Animator animator;
+    private bool isDying = false;  // Flag to prevent multiple executions
 
     void Start()
     {
+        animator = GetComponent<Animator>();
         _currentHealth = _maxHealth;
         _healthbar.UpdateHealthBar(_maxHealth, _currentHealth);
     }
 
     private void LateUpdate()
     {
-        if(_currentHealth <= 0)
+        if (_currentHealth <= 0 && !isDying) // Check if already dying
         {
-            GameObject pickup = Instantiate(collectable, transform.position, Quaternion.identity);
-            Vector3 collectablePosition = transform.position;
-            collectablePosition.x = transform.position.x;
-            collectablePosition.z = transform.position.z;
-            pickup.transform.position = collectablePosition;
-            Destroy(gameObject);
+            isDying = true; // Set the flag. Now it won't execute again. 
+            StartCoroutine(dying());
         }
+    }
+
+    private IEnumerator dying()
+    {
+        GameObject pickup = Instantiate(collectable, transform.position, Quaternion.identity);
+        animator.SetBool("isdead", true);
+        yield return new WaitForSeconds(2.0f);
+        Destroy(gameObject);
     }
 }
