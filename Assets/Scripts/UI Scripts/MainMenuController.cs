82d8825f0079639c666ae8d99084e463f1703409
Alexander Phipps <aphipps87@ymail.com>
2025-04-28 10:09:10 -0400

    menu fixes
    added level select control
    fixed menu overlap
    fixed ui for xp

--- Assets/Scripts/UI Scripts/MainMenuController.cs
+++ Assets/Scripts/UI Scripts/MainMenuController.cs
@@ -5,21 +5,44 @@ public class MainMenuController : MonoBehaviour
 {
 
     public CanvasGroup OptionPanel;
+    public CanvasGroup LevelSelect;
 
     public void PlayGame()
     {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        LevelSelect.alpha = 1;
+        LevelSelect.interactable = true;
+        LevelSelect.blocksRaycasts = true;
+    }
+
+    public void Mage()
+    {
+        SceneManager.LoadScene("FrozenLevel");
+    }
+
+    public void Brawler()
+    {
+        SceneManager.LoadScene("ForestLevel");
+    }
+
+    public void Rogue()
+    {
+        SceneManager.LoadScene("SandLevel");
     }
 
     public void Option()
     {
         OptionPanel.alpha = 1;
+        OptionPanel.interactable = true;
         OptionPanel.blocksRaycasts = true;
     }
     public void Back()
     {
         OptionPanel.alpha = 0;
-        OptionPanel.blocksRaycasts = true;
+        OptionPanel.blocksRaycasts = false;
+        OptionPanel.interactable = false;
+        LevelSelect.alpha = 0;
+        LevelSelect.blocksRaycasts = false;
+        LevelSelect.interactable = false;
     }
 
     public void QuitGame()
