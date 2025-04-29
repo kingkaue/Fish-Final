d456c9876c7481898f210c5062e32125270e4451
Alexander Phipps <aphipps87@ymail.com>
2025-04-28 23:47:48 -0400

    pause fixes

--- Assets/Scripts/UI Scripts/PauseController.cs
+++ Assets/Scripts/UI Scripts/PauseController.cs
@@ -10,33 +10,34 @@ public class PauseController : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.Escape))
         {
-            if (isPaused)
-            {
-                Back();
-            }
-            else
-            {
-                Pause();
-            }
+            if (isPaused) Back();
+            else Pause();
         }
     }
 
     public void Pause()
     {
-        PauseMenu.alpha = 1;
+        PauseMenu.alpha = 1f;
         PauseMenu.blocksRaycasts = true;
+        PauseMenu.interactable = true;
+
+        Time.timeScale = 0f;    // freeze everything
         isPaused = true;
     }
 
     public void Back()
     {
-        PauseMenu.alpha = 0;
+        PauseMenu.alpha = 0f;
         PauseMenu.blocksRaycasts = false;
+        PauseMenu.interactable = false;
+
+        Time.timeScale = 1f;    // resume normal time
         isPaused = false;
     }
 
     public void Menu()
     {
+        Time.timeScale = 1f;    // ensure time is running when you leave
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
     }
 }
